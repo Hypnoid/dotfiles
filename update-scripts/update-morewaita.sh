83eac8bd7e327275e5b2058de7ef9ad6f3f84ef9
Hypnoid <116501325+Hypnoid@users.noreply.github.com>
2023-10-08 19:43:58 +0200

    Upload Morewaita updater/installer

---
+++ update-scripts/update-morewaita.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# This script installs/updates the Morewaita icon theme
+
+	## Installation
+
+#? Clone the theme's git repo
+git clone https://github.com/somepaulo/MoreWaita.git
+cd Morewaita
+#? Run the theme's installer
+source install.sh
+
