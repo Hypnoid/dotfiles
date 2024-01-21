774fe70ad53a6a047c3af9f76771521d9e3a6894
Hypnoid <116501325+Hypnoid@users.noreply.github.com>
2024-01-21 17:26:41 +0100

    Rename Bash config and change it a little

--- bash/bash-config.sh
+++ bash/bash-config.sh
@@ -1,4 +1,5 @@
-#!  This is an extension of the .bashrc file, where I customize bash. I separate the two just to keep things clean.
+#! /usr/bin/env sh
+#  This is an extension of the .bashrc file, where I customize bash. I separate the two just to keep things clean.
 
 #?  Set up bash to use Starship
 eval "$(starship init bash)"
@@ -13,17 +14,15 @@ set -o vi
 alias sudo="sudo -p '$(printf "\e[31m\e[41m\e[97mBitte bestätigen Sie mit dem Passwort des Superusers:\e[m\e[31m\e[m\n")'"	# German version
 
 alias fp="flatpak"
-alias ro="rpm-ostree"
-alias tb="toolbox"
-alias vim="toolbox run vim"
+alias gte="flatpak run org.gnome.TextEditor"
 
 alias up=" \
-	printf ' \e[1m\e[4mFlatpak Update:\e[0m\n' && read -s && \
-	flatpak update ; \
-	printf '\n \e[1m\e[4mToolbox Update:\e[0m\n' && read -s && \
-	toolbox run sudo dnf upgrade ; \
-	printf '\n \e[1m\e[4mSystem Update:\e[0m\n' && read -s && \
-	rpm-ostree upgrade \
+	printf ' \e[1m\e[4mSystem Update:\e[0m\n' && read -s && \
+	sudo dnf upgrade ; \
+	printf '\n \e[1m\e[4mSystem Cleaning:\e[0m\n' && read -s && \
+	sudo dnf autoremove ; sudo dnf clean all ; \
+	printf '\n \e[1m\e[4mFlatpak Update:\e[0m\n' && read -s && \
+	flatpak update \
 	"
 alias upman=" \
 	printf ' \e[1m\e[4mUpdate Firefox Theme:\e[0m\n' && read -s && \
@@ -34,5 +33,5 @@ alias upman=" \
 	sh ~/Desktop/GitHub/dotfiles/update-scripts/update-gtk3-theme.sh ; \
 	"
 
-alias mu="toolbox run yt-dlp -x --embed-metadata --embed-thumbnail --ppa 'ThumbnailsConvertor+FFmpeg_o:-vf crop="in_h:in_h"' -o '%(track)s – %(artist)s'"
+alias mu="yt-dlp -x --embed-metadata --embed-thumbnail --ppa 'ThumbnailsConvertor+FFmpeg_o:-vf crop="in_h:in_h"' -o '%(track)s – %(artist)s'"
 
